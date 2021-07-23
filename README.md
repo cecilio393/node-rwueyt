3c4a8fab9fbc6d0943b0eeee4fffa3382bb79aa2
cecilio393 <basefx@mail.com>
2021-07-23 19:53:57 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# node-rwueyt
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/node-rwueyt)
---
+++ index.js
@@ -0,0 +1,3 @@
+l// run `node index.js` in the terminal
+
+console.log(`Hello Node.js v${process.versions.node}!`);
